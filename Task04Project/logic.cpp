2c8619d8561e1c3473365089ba4acceffd428e7d
darya-lobova <dasha.lobova.it@gmail.com>
2026-02-09 18:35:19 +0300

    Solved task 03

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -1,5 +1,11 @@
 ﻿#include "logic.h"
 
 string get_numbers_in_order(int n, int m) {
-	return "";
+	string result = "";
+
+	for (int i = n; i <= m; i++) {
+		result += to_string(i) + " ";
+	}
+
+	return result;
 }
