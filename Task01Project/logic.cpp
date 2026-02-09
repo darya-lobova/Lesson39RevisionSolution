d29aa477cb0d8486d003470f94488dfb88a572fb
darya-lobova <dasha.lobova.it@gmail.com>
2026-02-09 17:10:10 +0300

    Add project files.

---
+++ Task01Project/logic.cpp
@@ -0,0 +1,18 @@
+﻿#include "logic.h"
+
+string count_likes(int day, int like) {
+
+	if (day <= 0 || like < 0) {
+		return "Error.";
+	}
+
+	string result;
+
+	for (int i = 1; i <= day; i++) {
+		int final_likes = like * i;
+		result += "Day " + to_string(i) + ": " + to_string(final_likes) + " likes\n";
+
+	}
+
+	return result;
+}
