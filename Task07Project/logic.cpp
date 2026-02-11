0e55771e306798bcb6e0bc0441f326fb3b453d14
darya-loboca <dasha.lobova.it@gmail.com>
2026-02-11 20:36:33 +0300

    task 07 solved

--- Task07Project/logic.cpp
+++ Task07Project/logic.cpp
@@ -1,5 +1,24 @@
-﻿#include "logic.h"
+﻿#include <string>
 
-string get_prime_numbers(int number) {
-	return "";
+using namespace std;
+
+string get_prime_numbers(int n) {
+	if (n < 2) return "Error.";
+
+	string result = "";
+	for (int i = 2; i <= n; i++) {
+		bool is_prime = true;
+		for (int j = 2; j * j <= i; j++) {
+			if (i % j == 0) {
+				is_prime = false;
+				break;
+			}
+		}
+
+		if (is_prime) {
+			if (result != "") result += " ";
+			result += to_string(i);
+		}
+	}
+	return result;
 }
