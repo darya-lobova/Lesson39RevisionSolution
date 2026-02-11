05d9a3aaa5749f6ba3d12c178adce99a2c4f46fc
darya-loboca <dasha.lobova.it@gmail.com>
2026-02-11 20:40:47 +0300

    task 06 solved

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -1,5 +1,31 @@
-﻿#include "logic.h"
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
 
 long long get_fibo_number(int index) {
-	return 0LL;
+
+    if (index <= 0) {
+        return -1;
+    }
+
+    if (index == 1) {
+        return 0;
+    }
+
+    if (index == 2) {
+        return 1;
+    }
+
+    long long first = 0; 
+    long long second = 1; 
+    long long next = 0;
+
+    for (int i = 3; i <= index; i++) {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return second;
 }
