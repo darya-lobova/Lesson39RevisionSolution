145bbcfa9b96441a27cfca2da354cc2e3e72b7c0
darya-loboca <dasha.lobova.it@gmail.com>
2026-02-11 20:52:44 +0300

    task x solved

--- TaskXProject/logic.cpp
+++ TaskXProject/logic.cpp
@@ -1,5 +1,28 @@
 ﻿#include "logic.h"
 
 string build_pyramid(int row, char sign) {
-	return "";
+
+	if (row <= 0 || sign == ' ') {
+		return "Error.";
+	}
+
+	string pyramid = "";
+
+	for (int i = 1; i <= row; i++) {
+
+		for (int j = 0; j < row - i; j++) {
+			pyramid += " ";
+		}
+
+
+		for (int j = 0; j < (2 * i - 1); j++) {
+			pyramid += sign;
+		}
+
+		if (i < row) {
+			pyramid += "\n";
+		}
+	}
+
+	return pyramid;
 }
